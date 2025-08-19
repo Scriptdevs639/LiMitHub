6055ec5a6219f0fb23f5f559f0c95656b1ef7fdf
ScriptDevs <elonmaskwife@gmail.com>
2025-08-19 08:29:58 +0800

    Update PREMIUM.lua

--- PREMIUM.lua
+++ PREMIUM.lua
@@ -1,3 +1,2 @@
-‎ID="311c86f8-b442-42d8-a381-3c9473b1f92a";
-‎loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
 ‎loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/xYYgVS9dMZ9GxD1V/raw", true))()
